df59477c749cd5541ed04d230e907a29bc7de727
JayEpic <117071162+JayHzn@users.noreply.github.com>
2025-06-13 16:59:55 +0200

    Project begining + Firebase auth added + register finished

---
+++ amblyopie/android/app/src/main/kotlin/com/example/amblyopie/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.amblyopie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ amblyopie/lib/pages/auth/register_page.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class RegisterPage extends StatefulWidget{
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  String? _error;
+
+  Future<void> _register() async {
+    setState(() => _error = null);
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: _emailController.text.trim(),
+          password: _passwordController.text.trim(),
+        );
+
+      await FirebaseFirestore.instance
+        .collection('users')
+        .doc(userCredential.user!.uid)
+        .set({
+          'email': userCredential.user!.email,
+          'createdAt': FieldValue.serverTimestamp(),
+        });
+      
+      if(context.mounted) {
+        Navigator.pushReplacementNamed(context, '/home');
+      }
+  } on FirebaseAuthException catch (e) {
+    setState(() => _error = '[Auth:${e.code}] ${e.message}');
+  } on FirebaseException catch (e) {
+      // FirestoreException
+      setState(() => _error = '[Firestore:${e.code}] ${e.message}');
+  } catch (e) {
+      setState(() => _error = e.toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Inscription')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            if (_error != null)
+              Text(_error!, style: const TextStyle(color: Colors.red)),
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(labelText: 'Mot de passe'),
+              obscureText: true,
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: _register,
+              child: const Text('S\'inscrire')),
+          ],
+        ),
+      ),
+    );
+  }
+}
